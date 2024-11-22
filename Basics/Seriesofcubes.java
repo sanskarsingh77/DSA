5fdbba123e2957c1daf96993aa87aa4974a7bcdb
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2024-11-22 23:34:51 +0530

    Update Seriesofcubes.java

--- Basics/Seriesofcubes.java
+++ Basics/Seriesofcubes.java
@@ -1,16 +1,23 @@
 package Basic;
 
 import java.util.Scanner;
+
 public class SeriesOfCube 
 {
-    //1^3 + 2^3 + 3^3.....n^3
-    //In O(1) time
+    // Main method to calculate the sum of cubes of first n natural numbers
     public static void main(String[] args) 
     {
-       Scanner sc=new Scanner(System.in);   
+       Scanner sc = new Scanner(System.in);   
+       
+       // Input: The user inputs an integer n, representing the number of terms.
        System.out.print("Enter Number: ");
-       int n=sc.nextInt();  
-       int sumOfN = (n*(n+1))/2;
-       System.out.println("Sum of Series: "+(sumOfN * sumOfN));
+       int n = sc.nextInt();  
+
+       // Sum of First n Numbers: The sum of the first n natural numbers is calculated 
+       // using the formula n * (n + 1) / 2
+       int sumOfN = (n * (n + 1)) / 2;
+       
+       // Sum of Cubes: The sum of cubes is computed by squaring the sum of the first n numbers
+       System.out.println("Sum of Series: " + (sumOfN * sumOfN));
     }    
 }
