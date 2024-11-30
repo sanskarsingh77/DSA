40f4b45af63ec5261884ae7cd63dc02baadd2f79
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2024-11-30 23:10:33 +0530

    Update insertion.java

--- sorting/insertion.java
+++ sorting/insertion.java
@@ -1,3 +1,13 @@
+// Input Array: The unsorted array a is defined as {10, 70, 30, 40, 20}.
+
+// Sorting Logic:
+
+// The algorithm iterates over the array starting from the second element (i = 1), treating the portion before i as already sorted.
+// For each element (key = a[i]), it shifts elements in the sorted portion (a[0...i-1]) that are greater than key one position to the right to make space for key.
+// It then places key in its correct position within the sorted portion.
+// Output:
+
+// After sorting, the program prints the sorted array.
 public class Insertion 
 {
     public static void main(String[] args) 
