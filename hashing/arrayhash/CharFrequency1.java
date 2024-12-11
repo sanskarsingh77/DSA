c2c5b8709a88b061bb1058df05159e0d51b17e46
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2024-12-12 00:00:33 +0530

    Update CharFrequency1.java

--- hashing/arrayhash/CharFrequency1.java
+++ hashing/arrayhash/CharFrequency1.java
@@ -1,4 +1,16 @@
+// Initialization:
 
+// A string s containing lowercase letters is defined.
+// An integer array freq[26] is initialized to store letter frequencies (one index per letter from 'a' to 'z').
+// Counting Frequencies:
+
+// A loop goes through each character ch in the string s.
+// The character's ASCII value is used to determine its position in the array (ch - 97).
+// The corresponding index in freq is incremented.
+// Printing Results:
+
+// Another loop checks all elements in the freq array.
+// If a frequency is non-zero, the corresponding letter and its count are printed
 public class CharFreq1
 {
     public static void main(String[] args) 
