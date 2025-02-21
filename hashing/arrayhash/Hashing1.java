96c56bbbc4c3538511e65bc9ede6dd7ac8c4041f
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2025-02-21 23:42:10 +0530

    Update Hashing1.java

--- hashing/arrayhash/Hashing1.java
+++ hashing/arrayhash/Hashing1.java
@@ -1,1 +1,27 @@
 // Check if there are any two Equal numbers in an array at a distance less than or equal to k
+import java.util.*;
+public class Main{
+    public static boolean nearestDuplicate(int [] nums ,int k){
+        Map<Integer,Integer> numsIndices = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            // it checks 
+            if(numsIndices.containsKey(nums[i]) && i- numsIndices.get(nums[i]) <=k)
+            return true;
+            numsIndices.put(nums[i],i);
+        }
+         return false;
+    }
+   
+    
+}
+public static void Main(string[] args){
+    int [] nums ={3,2,1,3,3};
+    int k =3;
+    if(nearestDuplicate(nums,k)){
+        System.out.println("their are valid equal numbers within distance"+ k );
+    else{
+        System.out.println("their are no valid equal numbers within distance"+ k );
+        
+        }
+    }
+}
