ebc66e32618519ff208c22a1de6e33a23738f215
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2025-02-24 23:15:46 +0530

    Update hashing0.java

--- hashing/arrayhash/hashing0.java
+++ hashing/arrayhash/hashing0.java
@@ -5,6 +5,7 @@
 import java.util.*;
 public class Main {
   public static void main(String args[]) {
+
     Scanner sc = new Scanner(System.in);
     int n =sc.nextInt();
     int [] arr = new int[n];
